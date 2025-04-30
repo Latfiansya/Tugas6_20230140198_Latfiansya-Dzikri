1d8a161cf6093cddc48ccd8e9c855aa868229dbd
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-04-30 14:15:07 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("Video klip Bahasa Kalbu - Raisa");
+});
+
+audio.addEventListener("click", function audio() {
+    alert("Audio Bahasa Kalbu - Raisa");
+});
+
+gambar.addEventListener("click", function gambar() {
+    alert("Poster lagu Bahasa Kalbu - Raisa");
+});
